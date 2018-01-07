f27ed98c49d2f36ee0b56cd30a16fa3b26bca0f0
Yuan Xu <xuyuan@seas.upenn.edu>
2018-01-07 07:34:05 -0500

    erasing very very very bad and evil code

--- models/ProjectsDB.js
+++ models/ProjectsDB.js
@@ -13,26 +13,6 @@ var projectSchema = new Schema({
 
 var Project = mongoose.model('Project', projectSchema);
 
-// var readd = function(object) {
-// 	Project.remove({name: object.name}, function(e,d) {});
-// 	var parts = object.parts;
-// 	var newParts = [];
-// 	for (var d = 0; d < parts.length; d++) {
-// 		console.log("A part is " + parts[d]);
-// 		var part = parts[d].toLowerCase();
-// 		console.log(part);
-// 		newParts.push(part);
-// 	}
-
-// 	Project.create({name: object.name, parts: newParts, link: object.link}, function(err, dat) {});
-// }
-
-// Project.find({}, function(err, data) {
-// 	for (var i in data) {
-// 		readd(data[i]);
-// 	}
-// });
-
 module.exports = {
 	Project: Project
 }
